8a78cc61e1df12f09422bd9392228997ee01d9dc
PKarg <patryk.kargulewicz@gmail.com>
2022-11-14 15:48:31 +0100

    set up initial view prototypes

--- harvest/views.py
+++ harvest/views.py
@@ -1,9 +1,32 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
-
 
 def index(request: HttpRequest):
-    # TODO Implement
+    # TODO implement
     return HttpResponse("Home")
+
+
+def sign_up(request: HttpRequest):
+    # TODO implement
+    return HttpResponse("Sign up")
+
+
+def harvest_list(request: HttpRequest):
+    # TODO implement
+    return HttpResponse("Harvest list")
+
+
+def harvest_add(request: HttpRequest):
+    # TODO implement
+    return HttpResponse("Harvest add")
+
+
+def harvest_edit(request: HttpRequest):
+    # TODO implement
+    return HttpResponse("Harvest edit")
+
+
+def harvest_delete(request: HttpRequest):
+    # TODO implement
+    return HttpResponse("Harvest delete")
